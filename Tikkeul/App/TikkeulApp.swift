c5e09b6746287aef5cebb560e0966cd174f38db5
Jihun <75370733+jihun32@users.noreply.github.com>
2025-02-10 15:32:51 +0900

    Revert "Tikkeul 기록하는 뷰 UI 구현"

--- Tikkeul/App/TikkeulApp.swift
+++ Tikkeul/App/TikkeulApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct TikkeulApp: App {
+    let persistenceController = PersistenceController.shared
+
     var body: some Scene {
         WindowGroup {
-            TikkeulRootView()
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
