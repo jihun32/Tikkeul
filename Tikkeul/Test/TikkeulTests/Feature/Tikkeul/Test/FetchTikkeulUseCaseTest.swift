9e377e3db8abd0e5c5d933b4ff23820d89165b30
정지훈 <jihun332@gmail.com>
2025-02-24 10:37:31 +0900

    Test: 주간, 월간 단위로 데이터를 받아오는 테스트 코드 작성

--- Tikkeul/Test/TikkeulTests/Feature/Tikkeul/Test/FetchTikkeulUseCaseTest.swift
+++ Tikkeul/Test/TikkeulTests/Feature/Tikkeul/Test/FetchTikkeulUseCaseTest.swift
@@ -1,5 +1,5 @@
 //
-//  FetchTikkeulkUseCaseTest.swift
+//  FetchTikkeulUseCaseTest.swift
 //  Tikkeul
 //
 //  Created by 정지훈 on 2/13/25.
@@ -34,14 +34,84 @@ final class FetchTikkeulUseCaseTest: XCTestCase {
     
     // MARK: - Test Function
     
-    func test_fetchTikkeul함수호출시_오늘의데이터를받아오는지() throws {
+    func test_fetchTikkeul_오늘의데이터를가져오는지() throws {
         // Given
         let date = Date()
-        let expectedItems = getFilteredItemsByRange(by: date.startOfDay..<date.endOfDay, items: TikkeulData.dummyData)
+        let startOfDay = date.startOfDay
+        guard let endOfDay = date.endOfDay else {
+            XCTFail("endOfDay 계산 실패")
+            return
+        }
+        
+        let expectedItems = getFilteredItemsByRange(by: startOfDay..<endOfDay, items: TikkeulData.dummyData)
         
         // When
-        let resultItems = try sut.fetchTikkeul(from: date.startOfDay, to: date.endOfDay)
+        let resultItems = try sut.fetchTikkeul(from: startOfDay, to: endOfDay)
         
+        // Then
+        XCTAssertEqual(resultItems, expectedItems)
+    }
+    
+    func test_fetchTikkeul_저번주데이터를가져오는지() throws {
+        // Given
+        guard let lastWeekRange = Date().getWeekRange(weeksAgo: -1) else {
+            XCTFail("lastWeekRange 계산 실패")
+            return
+        }
+        
+        let expectedItems = getFilteredItemsByRange(by: lastWeekRange, items: TikkeulData.dummyData)
+
+        // When
+        let resultItems = try sut.fetchTikkeul(from: lastWeekRange.lowerBound, to: lastWeekRange.upperBound)
+
+        // Then
+        XCTAssertEqual(resultItems, expectedItems)
+    }
+
+    func test_fetchTikkeul_이번주데이터를가져오는지() throws {
+        // Given
+        guard let thisWeekRange = Date().getWeekRange(weeksAgo: 0) else {
+            XCTFail("thisWeekRange 계산 실패")
+            return
+        }
+        
+        let expectedItems = getFilteredItemsByRange(by: thisWeekRange, items: TikkeulData.dummyData)
+
+        // When
+        let resultItems = try sut.fetchTikkeul(from: thisWeekRange.lowerBound, to: thisWeekRange.upperBound)
+
+        // Then
+        XCTAssertEqual(resultItems, expectedItems)
+    }
+
+    func test_fetchTikkeul_저번달데이터를가져오는지() throws {
+        // Given
+        guard let lastMonthRange = Date().getMonthRange(monthsAgo: -1) else {
+            XCTFail("lastMonthRange 계산 실패")
+            return
+        }
+        
+        let expectedItems = getFilteredItemsByRange(by: lastMonthRange, items: TikkeulData.dummyData)
+
+        // When
+        let resultItems = try sut.fetchTikkeul(from: lastMonthRange.lowerBound, to: lastMonthRange.upperBound)
+
+        // Then
+        XCTAssertEqual(resultItems, expectedItems)
+    }
+
+    func test_fetchTikkeul_이번달데이터를가져오는지() throws {
+        // Given
+        guard let thisMonthRange = Date().getMonthRange(monthsAgo: 0) else {
+            XCTFail("thisMonthRange 계산 실패")
+            return
+        }
+        
+        let expectedItems = getFilteredItemsByRange(by: thisMonthRange, items: TikkeulData.dummyData)
+
+        // When
+        let resultItems = try sut.fetchTikkeul(from: thisMonthRange.lowerBound, to: thisMonthRange.upperBound)
+
         // Then
         XCTAssertEqual(resultItems, expectedItems)
     }
